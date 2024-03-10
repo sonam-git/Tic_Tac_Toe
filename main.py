caa59484bdb9511fc2b1e2a9df54c1810f39269f
Sonam Sherpa <sherpa.sjs@gmail.com>
2024-03-10 16:47:39 -0700

    This is my first commit

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print('hello world')
+board = ["X","O","X",
+         "O","X","O",
+         "X","O","X",]
+
+
+def print_board(board):
+    print(board[0] + " | " + board[1] + " | " + board[2])
+    print("----------")
+    print(board[3] + " | " + board[4] + " | " + board[5])
+    print("----------")
+    print(board[6] + " | " + board[7] + " | " + board[8])
+    print("----------")
+print_board(board)
