5a1f5560ec0ad843c1daca15aeb4387541cff4bd
Sonam Sherpa <sherpa.sjs@gmail.com>
2024-03-15 10:39:09 -0700

    checkWin and Switch player function created

--- main.py
+++ main.py
@@ -1,40 +1,100 @@
 # variable declaration
-board = ["-", "-", "-",
-         "-", "-", "-",
-         "-", "-", "-", ]
+game_board = ["-", "-", "-",
+              "-", "-", "-",
+              "-", "-", "-", ]
 currentPlayer = 'X'
 winner = None
 gameRunning = True
 
 
 # display board created
-def print_board(board):
-    print(board[0] + " | " + board[1] + " | " + board[2])
+def printBoard(game_board):
+    print(game_board[0] + " | " + game_board[1] + " | " + game_board[2])
     print("----------")
-    print(board[3] + " | " + board[4] + " | " + board[5])
+    print(game_board[3] + " | " + game_board[4] + " | " + game_board[5])
     print("----------")
-    print(board[6] + " | " + board[7] + " | " + board[8])
+    print(game_board[6] + " | " + game_board[7] + " | " + game_board[8])
     print("----------")
 
 
 # player input functionality
-def player_input(board):
-    playerInput = int(input('Enter a number 1 - 9: '))
-    if 1 <= playerInput <= 9 and board[playerInput - 1] == "-":
-        board[playerInput - 1] = currentPlayer
+def playerInput(game_board):
+    playerInp = int(input('Enter a number 1 - 9: '))
+    if 1 <= playerInp <= 9 and game_board[playerInp - 1] == "-":
+        game_board[playerInp - 1] = currentPlayer
     else:
         print("Opps! player is already in that spot!")
 
 
-player_input(board)
+playerInput(game_board)
 
 
-def win_check(board, mark):
-    return ((board[7] == mark and board[8] == mark and board[9] == mark) or  # across the top
-            (board[4] == mark and board[5] == mark and board[6] == mark) or  # across the middle
-            (board[1] == mark and board[2] == mark and board[3] == mark) or  # across the bottom
-            (board[7] == mark and board[4] == mark and board[1] == mark) or  # down the middle
-            (board[8] == mark and board[5] == mark and board[2] == mark) or  # down the middle
-            (board[9] == mark and board[6] == mark and board[3] == mark) or  # down the right side
-            (board[7] == mark and board[5] == mark and board[3] == mark) or  # diagonal
-            (board[9] == mark and board[5] == mark and board[1] == mark))  # diagonal
+# check win function
+# horizontal check
+def checkHorizontal(game_board):
+    global winner
+    if game_board[0] == game_board[1] == game_board[2] and game_board[1] != "-":
+        winner = game_board[0]
+        return True
+    elif game_board[3] == game_board[4] == game_board[5] and game_board[3] != "-":
+        winner = game_board[3]
+        return True
+    elif game_board[6] == game_board[7] == game_board[8] and game_board[6] != "-":
+        winner = game_board[6]
+        return True
+
+
+# row check
+def checkRow(game_board):
+    global winner
+    if game_board[0] == game_board[3] == game_board[6] and game_board[0] != "-":
+        winner = game_board[0]
+        return True
+    elif game_board[1] == game_board[4] == game_board[7] and game_board[1] != "-":
+        winner = game_board[1]
+        return True
+    elif game_board[2] == game_board[5] == game_board[8] and game_board[2] != "-":
+        winner = game_board[2]
+        return True
+
+
+# diagonal check
+def checkDag(game_board):
+    global winner
+    if game_board[0] == game_board[4] == game_board[8] and game_board[0] != "-":
+        winner = game_board[0]
+        return True
+    elif game_board[2] == game_board[4] == game_board[6] and game_board[2] != "-":
+        winner = game_board[2]
+        return True
+
+
+# tie check
+
+def checkTie(game_board):
+    if "-" not in game_board:
+        printBoard(game_board)
+        print("It is a tie!")
+        gameRunning = False
+
+
+def checkWin():
+    if checkDag(game_board) or checkHorizontal(game_board) or checkRow(game_board):
+        print(f"The winner is{winner}")
+
+
+# switch player
+def switchPlayer():
+    global currentPlayer
+    if currentPlayer == 'X':
+        currentPlayer = 'O'
+    else:
+        currentPlayer = 'X'
+
+
+while gameRunning:
+    print(game_board)
+    playerInput(game_board)
+    checkWin()
+    checkTie(game_board4)
+    switchPlayer()
